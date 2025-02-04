cc45a7b4303df551966a3ca12f50d6607a8031b2
salman-ctrl <zirmanvictory@gmail.com>
2025-02-04 14:16:58 +0700

    perubahan kecil

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 const Quiz = () => {
+
     return (
         <div className='w-150 shadow-gray-600 shadow-md  m-auto mt-24 bg-white text-gray-600 flex flex-col gap-6 rounded-md px-10 py-16'>
             <h1 className='flex justify-center -mb-2 font-bold text-4xl'>Quiz App</h1>
             <hr className='h-0.5 border-none bg-gray-500' />
             <h2 className='text-2xl font-semibold'> Which Device is required for the Internet connection?</h2>
             <ul className='list-alpha pl-5'>
-                <li>Modem</li>
+                <li>Remote</li>
                 <li>Router</li>
                 <li>LAN Cable</li>
                 <li>Pen Drive</li>
