9870bb23c11bec384c664242b81701cec61c515c
salman-ctrl <zirmanvictory@gmail.com>
2025-02-04 16:02:36 +0700

    Change a lot

---
+++ public/data.js
@@ -0,0 +1,82 @@
+export const data = [
+    {
+        question: "Which protocol is used to send emails?",
+        option1: "HTTP",
+        option2: "FTP",
+        option3: "SMTP",
+        option4: "IMAP",
+        answer: 3
+    },
+    {
+        question: "What does CPU stand for?",
+        option1: "Central Processing Unit",
+        option2: "Computer Personal Unit",
+        option3: "Central Performance Unit",
+        option4: "Control Processing Unit",
+        answer: 1
+    },
+    {
+        question: "Which language is primarily used for web development?",
+        option1: "Python",
+        option2: "C++",
+        option3: "JavaScript",
+        option4: "Swift",
+        answer: 3
+    },
+    {
+        question: "Which data structure uses the LIFO principle?",
+        option1: "Queue",
+        option2: "Stack",
+        option3: "Linked List",
+        option4: "Array",
+        answer: 2
+    },
+    {
+        question: "What does RAM stand for?",
+        option1: "Random Access Memory",
+        option2: "Read-Only Memory",
+        option3: "Rapid Access Memory",
+        option4: "Remote Access Memory",
+        answer: 1
+    },
+    {
+        question: "Which company developed the Windows operating system?",
+        option1: "Apple",
+        option2: "Microsoft",
+        option3: "Google",
+        option4: "IBM",
+        answer: 2
+    },
+    {
+        question: "Which of the following is NOT a programming language?",
+        option1: "HTML",
+        option2: "Java",
+        option3: "Python",
+        option4: "C#",
+        answer: 1
+    },
+    {
+        question: "What does HTTP stand for?",
+        option1: "HyperText Transfer Protocol",
+        option2: "Hyper Transfer Text Protocol",
+        option3: "HighText Transfer Protocol",
+        option4: "HyperText Transmission Protocol",
+        answer: 1
+    },
+    {
+        question: "Which database language is used to manage and query databases?",
+        option1: "SQL",
+        option2: "JavaScript",
+        option3: "Python",
+        option4: "C++",
+        answer: 1
+    },
+    {
+        question: "Which one of these is an open-source operating system?",
+        option1: "Windows",
+        option2: "Linux",
+        option3: "macOS",
+        option4: "iOS",
+        answer: 2
+    }
+];
