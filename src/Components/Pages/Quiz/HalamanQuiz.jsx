7aa5b869c0c379ed2594771ca02e8ff97e9bfe8b
salman-ctrl <zirmanvictory@gmail.com>
2025-02-07 02:23:53 +0700

    perubahan kecil

--- src/Components/Pages/Quiz/HalamanQuiz.jsx
+++ src/Components/Pages/Quiz/HalamanQuiz.jsx
@@ -12,8 +12,18 @@ const HalamanQuiz = () => {
                         <h2 className="text-white flex pt-4 justify-center">Quiz level 2</h2>
                     </div>
                 </Link>
-
-
+                <Link to="/quiz/1">
+                    <div className="h-64 rounded-md bg-blue-800 w-72 mt-10 m-auto">
+                        <img className="h-50 w-full rounded-md" src="/src/assets/foto.png" alt="" />
+                        <h2 className="text-white flex pt-4 justify-center">Quiz level 2</h2>
+                    </div>
+                </Link>
+                <Link to="/quiz/1">
+                    <div className="h-64 rounded-md bg-blue-800 w-72 mt-10 m-auto">
+                        <img className="h-50 w-full rounded-md" src="/src/assets/foto.png" alt="" />
+                        <h2 className="text-white flex pt-4 justify-center">Quiz level 2</h2>
+                    </div>
+                </Link>
 
 
 
