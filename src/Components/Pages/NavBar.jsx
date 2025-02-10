ead184dfbfb2681013d0f815fae27f9a4eb3a2ce
salman-ctrl <zirmanvictory@gmail.com>
2025-02-10 23:41:13 +0700

    edit navbar

--- src/Components/Pages/NavBar.jsx
+++ src/Components/Pages/NavBar.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import { useContext } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { userContext } from '../../context/userContext'
+
 
 const NavBar = () => {
+    const { nama } = useContext(userContext)
+
     return (
-        <div className='w-full h-20 bg-gray-500 flex justify-center gap-x-5 items-center pr-10'>
-            <p>Home</p>
-            <p>Elearning</p>
-            <p>Kontak</p>
-            <p>Akun Saya</p>
+        <div className='w-full h-20 bg-gray-500 flex justify-between gap-x-5 items-center pr-10 px-20'>
+
+            <div className='flex gap-x-10 '>
+                <p>Home</p>
+                <p>Elearning</p>
+            </div>
+            <div className='flex gap-x-10'>
+                <p>Kontak</p>
+                <p>Akun Saya</p>
+                <div className='flex gap-x-3'>
+                    <p>hai, {nama}!</p>
+                    <FontAwesomeIcon icon={faUser} className='pt-1' />
+
+                </div>
+
+            </div>
+
         </div>
     )
 }
