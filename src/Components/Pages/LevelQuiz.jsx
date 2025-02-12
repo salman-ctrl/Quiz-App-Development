ddcea723737cc45f01771d60c1999033f2c2f550
salman-ctrl <zirmanvictory@gmail.com>
2025-02-12 21:57:32 +0700

    perbaiki halaman level

--- src/Components/Pages/LevelQuiz.jsx
+++ src/Components/Pages/LevelQuiz.jsx
@@ -9,7 +9,7 @@ const LevelQuiz = () => {
             <h1>Pilih Level</h1>
             <div className='w-52 h-36 bg-sky-500'>
                 <button className='bg-green-400 px-4 py-2' >Easy</button>
-                <button >Medium</button>
+                <button className='bg-orange-300 px-4 py-2'>Medium</button>
                 <button>Hard</button>
             </div>
         </div>
