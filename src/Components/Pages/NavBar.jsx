fff9e4d9984974a025bbff37b4ef8d83fe84f70f
salman-ctrl <zirmanvictory@gmail.com>
2025-02-12 21:51:27 +0700

    tambah data

--- src/Components/Pages/NavBar.jsx
+++ src/Components/Pages/NavBar.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useContext } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-regular-svg-icons'
@@ -9,7 +8,7 @@ const NavBar = () => {
     const { nama } = useContext(userContext)
 
     return (
-        <div className='w-full h-24 bg-gray-600/80 flex justify-end bg-gray-600/80 backdrop-blur-md transition-all shadow-md gap-x-5 items-center pr-10 px-20 text-xl  font-semibold sticky top-0 z-30 text-white'>
+        <div className='w-full h-24  flex justify-end bg-gray-600/80 backdrop-blur-md transition-all shadow-md gap-x-5 items-center pr-10 px-20 text-xl  font-semibold sticky top-0 z-30 text-white'>
             <div className='flex gap-x-10'>
                 <p>Kontak</p>
                 <div className='flex gap-x-3 cursor-pointer'>
