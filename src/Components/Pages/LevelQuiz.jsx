54398d3b6ffa83c8439c2a637b3efff1eba22856
salman-ctrl <zirmanvictory@gmail.com>
2025-02-12 22:05:49 +0700

    edit button

--- src/Components/Pages/LevelQuiz.jsx
+++ src/Components/Pages/LevelQuiz.jsx
@@ -10,7 +10,7 @@ const LevelQuiz = () => {
             <div className='flex justify-evenly mt-7 '>
                 <button className='bg-green-400 w-24 rounded-md' >Easy</button>
                 <button className='bg-orange-300 w-24 rounded-md '>Medium</button>
-                <button className='bg-red-600 w-24 py-2'>Hard</button>
+                <button className='bg-red-600 w-24 py-2 rounded-md'>Hard</button>
             </div>
         </div>
     )
