b967e27e45b942b76562e82c7f40ea634ed81979
salman-ctrl <zirmanvictory@gmail.com>
2025-02-15 20:30:33 +0700

    fixing bug lagi

--- src/Components/Pages/LevelQuiz.jsx
+++ src/Components/Pages/LevelQuiz.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { Link } from "react-router-dom";
 import { userContext } from '../../Context/userContext';
+import NavBar from './NavBar';
 
 const LevelQuiz = () => {
 
@@ -13,14 +14,18 @@ const LevelQuiz = () => {
     }, [level]);
 
     return (
-        <div className='w-96 h-36 bg-sky-500 m-auto rounded-md '>
-            <h1 className='text-center pt-3'>Pilih Level</h1>
-            <div className='flex justify-evenly mt-7 font-bold text-gray-700'>
-                <Link to='/dashboard/quiz/:id/detail' ><button onClick={() => setLevel('easy')} className='bg-green-400 py-2 w-24 rounded-md'>Easy</button> </Link>
-                <Link to='/dashboard/quiz/:id/detail'  > <button onClick={() => setLevel('medium')} className='bg-orange-500 py-2 w-24 rounded-md '>Medium</button> </Link>
-                <Link to='/dashboard/quiz/:id/detail' > <button onClick={() => setLevel('hard')} className='bg-red-600 w-24 py-2 rounded-md' >Hard</button> </Link>
-            </div>
-        </div>
+        <div className='h-screen bg-gradient-to-br from-sky-200 to-purple-800' >
+            <NavBar />
+            <div className='w-96 h-36 bg-sky-300 m-auto rounded-md mt-40 shadow-2xl'>
+                <h1 className='text-center pt-3'>Pilih Level </h1>
+                <div className='flex justify-evenly mt-7 font-bold text-gray-700'>
+                    <Link to='/dashboard/quiz/:id/detail' ><button onClick={() => setLevel('easy')} className='bg-green-400 py-2 w-24 rounded-md'>Easy</button> </Link>
+                    <Link to='/dashboard/quiz/:id/detail'  > <button onClick={() => setLevel('medium')} className='bg-orange-500 py-2 w-24 rounded-md '>Medium</button> </Link>
+                    <Link to='/dashboard/quiz/:id/detail' > <button onClick={() => setLevel('hard')} className='bg-red-600 w-24 py-2 rounded-md' >Hard</button> </Link>
+                </div>
+            </div >
+        </div >
+
     )
 }
 
